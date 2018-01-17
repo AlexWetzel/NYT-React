be6db00f97ef7bdabf9eb090085691af01a331ed
AlexWetzel <wetzela@live.com>
2018-01-16 19:07:21 -0500

    Troubleshooting

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Serve up static assets
-// app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 // Add routes, both API and view
 app.use("", routes);
 
@@ -25,7 +27,9 @@ mongoose.connect(
 );
 
 
-
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 // Start the API server
 app.listen(PORT, function() {
